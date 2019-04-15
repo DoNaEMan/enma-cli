780ac32e3e4ae2ba64af2b62ff1a0f62c274cca1
za-wangqin <qin.wang@zatech.com>
2019-04-15 16:13:56 +0800

    增加可选则仓库

--- index.js
+++ index.js
@@ -21,11 +21,15 @@ program.version('1.0.0', '-v, --version')
         {
           name: 'author',
           message: '请输入作者名称'
+        },
+        {
+          name: 'src',
+          message: '请输入模板的git仓库地址'
         }
       ]).then((answers) => {
         const spinner = ora('正在下载模板...')
         spinner.start()
-        download('https://github.com:DoNaEMan/enma#master', name, {clone: true}, (err) => {
+        download(answers.src || 'https://github.com:DoNaEMan/enma#master', name, {clone: true}, (err) => {
           if (err) {
             spinner.fail()
             console.log(symbols.error, chalk.red(err))
